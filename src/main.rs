e595b8921d5b22fdd2c1394e87ec9ee4954a7ec3
HK416 <powerspirit127@gmail.com>
2023-07-26 11:17:21 +0900

    Written the code to open the window screen using winit crate.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use winit::{
+    event_loop::EventLoop,
+    window::WindowBuilder, 
+    event::{Event, WindowEvent},
+};
+
+
 fn main() {
-    println!("Hello, world!");
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new()
+        .with_title("Temp")
+        .build(&event_loop)
+        .expect("Window creation failed!");
+
+    event_loop.run(move |event, _, control_flow| {
+        control_flow.set_wait();
+
+        match event {
+            Event::WindowEvent { window_id, event } 
+            if window_id == window.id() => match event {
+                WindowEvent::CloseRequested => {
+                    control_flow.set_exit();
+                },
+                _ => { }
+            },
+            Event::MainEventsCleared => {
+                window.request_redraw();
+            },
+            Event::RedrawRequested(_) => {
+                // TODO
+            },
+            _ => { },
+        }
+    });
 }
